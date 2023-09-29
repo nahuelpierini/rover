290a7e1204f92034a2b7d9fc7769e788c83da200
Nahuel Pierini <nahuelpierini@gmail.com>
2023-09-29 15:14:51 +0200

    edit front

--- rover/src/main/resources/static/script.js
+++ rover/src/main/resources/static/script.js
@@ -1,28 +1,31 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    createMap();
+});
+
 let mapId = 1;
 let roverId = 6;
+let coordinateId = 18;
 let commandString = '';
 
 async function createMap() {
     try {
         const response = await fetch(`/api/map/${mapId}`);
         if (!response.ok) {
-            throw new Error('Error en la solicitud al servidor');
+            throw new Error('Map server request error');
         }
         
         const data = await response.json();
         
-
         const width = data.width;
         const height = data.height;
         
-
         createMapWithDimensions(width, height);
 
  
          const obstaclesResponse = await fetch('/api/obstacle');
          if (!obstaclesResponse.ok) {
-             throw new Error('Error en la solicitud de obstáculos al servidor');
+             throw new Error('Obstacle server request error');
          }
  
          const obstaclesData = await obstaclesResponse.json();
@@ -33,15 +36,13 @@ async function createMap() {
 
          const roverInitialPositionResponse = await fetch(`/api/rover/${roverId}`);
          if (!roverInitialPositionResponse.ok) {
-             throw new Error('Error en la solicitud de posición inicial al servidor');
+             throw new Error('Rover initial position server request error');
          }
  
          const roverInitialPositionData = await roverInitialPositionResponse.json();
  
          assignInitialPositionToRobot(roverInitialPositionData);
  
-
-
     } catch (error) {
         console.error('Error:', error);
     }
@@ -61,11 +62,8 @@ function displayObstacles(obstacles) {
         const obstacleImage = document.createElement('img');
         obstacleImage.setAttribute('src', 'images/rock.png');
         obstacleImage.setAttribute('class', 'rock');
-        obstacleImage.style.position = 'absolute';
-        obstacleImage.style.width = '100px';
         obstacleImage.style.left = (obstacle.coordinateX*100) + 'px';
         obstacleImage.style.top = (obstacle.coordinateY*100) + 'px';
-        
         container.appendChild(obstacleImage);
     });
 }
@@ -79,85 +77,55 @@ function assignInitialPositionToRobot(roverPositionData) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    createMap();
-});
-
-
-
-
-
-
-// Función para agregar comandos a la cadena y mostrar en la consola
 function addCommandToConsole(command) {
     commandString += command;
     const commandTextarea = document.getElementById('command-textarea');
     commandTextarea.value = commandString;
 }
 
-// Función para enviar los comandos al backend (puedes implementar esto más tarde)
-function sendCommands() {
-    // Aquí puedes enviar 'commandString' al backend a través de una solicitud AJAX o como prefieras.
-    console.log('Comandos enviados al backend:', commandString);
-}
-
-// Modifica tus funciones de control para agregar comandos a la consola
 function moveForward() {
-    // Lógica para mover hacia adelante
-    // ...
 
-    // Agrega el comando 'F' a la consola
     addCommandToConsole('F');
 }
 
 
 function clickBtnRotateLeft() {
-    // Lógica para girar a la izquierda
-    // ...
 
-    // Agrega el comando 'L' a la consola
     addCommandToConsole('L');
 }
 
 function clickBtnRotateRight() {
-    // Lógica para girar a la derecha
-    // ...
 
-    // Agrega el comando 'R' a la consola
     addCommandToConsole('R');
 }
 
 function clearConsole() {
-    // Limpia la cadena de comandos y la consola
     commandString = '';
     const commandTextarea = document.getElementById('command-textarea');
     commandTextarea.value = '';
 }
 
-
 async function sendCommands() {
-    // Obtiene los comandos de la consola
+
     const commandTextarea = document.getElementById('command-textarea');
     const commandString = commandTextarea.value;
 
     try {
-        // Realiza una solicitud POST al backend para guardar los comandos
-        const response = await fetch('/api/coordinates/18', {
+
+        const response = await fetch(`/api/coordinates/${coordinateId}`, {
             method: 'PUT',
             headers: {
-                'Content-Type': 'text/plain', // Cambia el tipo de contenido a "text/plain"
+                'Content-Type': 'text/plain',
             },
-            body: commandString, // Envia directamente la cadena de comandos
+            body: commandString, 
         });
 
         if (!response.ok) {
-            throw new Error('Error al enviar los comandos al servidor');
+            throw new Error('Commands server error request');
         }
 
-        // Limpia la consola después de enviar los comandos
         clearConsole();
 
-        // Después de enviar los comandos y actualizar las coordenadas, mueve el rover
         moveRover();
 
     } catch (error) {
@@ -168,57 +136,20 @@ async function sendCommands() {
 
 async function moveRover() {
     try {
-        // Realiza una solicitud GET al backend para obtener la posición actual del rover
-        const roverPositionResponse = await fetch(`/api/rover/6`);
-        if (!roverPositionResponse.ok) {
-            throw new Error('Error al obtener la posición del rover');
-        }
-
-        const roverPositionData = await roverPositionResponse.json();
-        
-        // Actualiza la posición del rover en el frontend
-        updateRoverPosition(roverPositionData);
-
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-function updateRoverPosition(roverPositionData) {
-    const roverElement = document.getElementById('rover');
-    roverElement.style.left = (roverPositionData.newPositionX * 100) + 'px';
-    roverElement.style.top = (roverPositionData.newPositionY * 100) + 'px';
-    // También puedes ajustar la orientación del rover aquí si es necesario
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// Define la función para mover el rover
-async function moveRover() {
-    try {
         // Realiza una solicitud PUT al endpoint moveRover de tu backend
-        const response = await fetch('/api/rover/6/map/1/coordinates/18', {
+        const response = await fetch(`/api/rover/${roverId}/map/${mapId}/coordinates/${coordinateId}`, {
             method: 'PUT', // Utiliza el método PUT para enviar los comandos
         });
 
         if (!response.ok) {
-            throw new Error('Error al mover el rover');
+            throw new Error('Rover movement error');
         }
 
         // Actualiza la posición del rover en el frontend
         updateRoverPosition();
 
     } catch (error) {
-        console.error('Error al mover el rover:', error);
+        console.error('Rover movement error:', error);
     }
 }
 
@@ -226,9 +157,9 @@ async function moveRover() {
 async function updateRoverPosition() {
     try {
         // Realiza una solicitud GET para obtener la posición actual del rover
-        const roverPositionResponse = await fetch('/api/rover/6'); // Reemplaza '1' con el ID correcto del rover
+        const roverPositionResponse = await fetch(`/api/rover/${roverId}`); // Reemplaza '1' con el ID correcto del rover
         if (!roverPositionResponse.ok) {
-            throw new Error('Error al obtener la posición del rover');
+            throw new Error('Rover position server error request');
         }
 
         const roverPositionData = await roverPositionResponse.json();
